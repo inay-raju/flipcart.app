b6986a6bd14cfe5cef48c8d8eb02ebbb34eec1a3
vinayraju <vinaykumarrau27@gmail.com>
2024-11-08 16:13:26 +0530

    this the flipcart.java file code

---
+++ flipcart1.java
@@ -0,0 +1,13 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        // Loop through the first 10 numbers (1 to 10)
+        for (int i = 1; i <= 10; i++) {
+            sum += i;  // Add i to sum
+        }
+
+        // Print the sum
+        System.out.println("The sum of the first 10 numbers is: " + sum);
+    }
+}
